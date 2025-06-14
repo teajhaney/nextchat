160f242f5981664250a6941064ef89c10523effa
Yusuf <teajhaney@gmail.com>
2025-06-15 00:27:00 +0100

    refactor: update font imports and styles in layout and globals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter, Lato } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const inter = Inter({
+  variable: '--font-inter',
   subsets: ['latin'],
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const lato = Lato({
+  variable: '--font-lato',
   subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '700', '900'],
 });
 
 export const metadata: Metadata = {
@@ -24,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${inter.variable} ${lato.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-
-
 export default function Home() {
-  return (
-    <div >
-     home
-    </div>
-  );
+  return <div className="font-lato">home</div>;
 }
