ffbff1d13faf0d2703addcdca0604351914c5d41
Yusuf <teajhaney@gmail.com>
2025-06-21 20:05:35 +0100

    refactor: remove unused imports and initial setup for message store

--- src/app/store/messageStore.ts
+++ src/app/store/messageStore.ts
@@ -1,10 +1,4 @@
-// import {  MessageState } from '@/index';
-// import { create } from 'zustand';
 
-// export const useMessageStore = create<MessageState>(set => ({
-//   selectedChatUser: null,
-//   setSelectedChatUser: (user) => set({ selectedChatUser: user }),
-// }));
 
 import { create } from 'zustand';
 import { fetchMessages, sendMessage } from '@/lib/messages/messageService';
